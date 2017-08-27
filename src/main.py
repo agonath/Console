04007ef9c54a7ccdeca47acc06c910fdacca6022
Mirko <Mirko.braun.frankfurt@gmx.de>
2017-08-27 22:18:02 +0200

    SystemLoader Klasse erweitert. Klassen/Modul import aus relativen Pfaden ist jetzt möglich.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from console import Console
-from loader import systemLoader
+from loader import SystemLoader
 
 
 # Test-Command
@@ -25,12 +25,20 @@ def main(args):
     con = Console(args)
    # hello = Hello_World(_name="hello", _console=con)
     
-    load = systemLoader() 
+    load = SystemLoader() 
     # try to create the class object
-    newClass = load.loadClassFromModule(_className="Hello_World", _moduleName="Commands")
+    newClass = load.load_ModuleAndGetClass(_className="Hello_World", _moduleName="Hello_World", _path=str(".\\" + os.sep + "Commands"))
     hello = newClass(_name="hello", _console=con)
     # register the command...
     con.registerCommand(hello)
+
+    newClass2 = load.load_ModuleAndGetClass(_path="..\\bin\Plugins\\Commands", _moduleName="Hello_World2.pyc", _className="Hello_World2")
+    hello2 = newClass2(_name="hello2", _console=con)
+    con.registerCommand(hello2)
+
+    print(chr(0x2557))
+    print(chr(0x2500))
+    print(oct(0x2500))
     
     
     con.consoleLoop();
